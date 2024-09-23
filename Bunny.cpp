9970ab70105d02f509baeebd5b2aa7057d30cd3d
Grobanix <124403888+Grobanix@users.noreply.github.com>
2024-09-23 21:43:11 +0200

    Update Bunny.cpp

--- Bunny.cpp
+++ Bunny.cpp
@@ -3,22 +3,24 @@
 #include <vector>
 #include <iostream>
 
-bool Condition{0};
+short Order{0};
 int Statements{-1};
 int Tokens{-1};
 int Block{0};
 
 enum TokenType {
-	Number, String, Variable
+	None ,Number, String, Variable, Operator, Function, Condition , Standard
 };
 
 struct Token {
-	enum TokenType Type{};
+	enum TokenType stType{};
+	enum TokenType ndType{};
+	enum TokenType rdType{};
 	std::string Content{""};
 	int Block{0};
 	
 	Token(TokenType Type)
-	{ Condition = 1; ++Tokens; this->Type = Type; }
+	{ Order = 1; ++Tokens; this->stType = Type; }
 };
 
 class FileManager {
@@ -32,15 +34,33 @@ class FileManager {
 
 void Parse(std::string& Line, std::vector<Token>& Statement) {
 	for (int i{0}; i < Line.length(); i++) {
-		if (Condition) {
-			if (Line.at(i) == ' ') { Condition = 0; }
-			else { Statement.at(Tokens).Content += Line.at(i); }
-		} else {
+		switch (Order) {
+			case 2:
+			switch (Line.at(i)) {
+				case ' ': { Order = 0; } break;
+				case '~': { Statement.at(Tokens).rdType = Standard; } break;
+				default: { Statement.at(Tokens).Content += Line.at(i); }
+			}
+			break;
+			case 1:
+			switch (Line.at(i)) {
+				case ' ': { Order = 0; } break;
+				case '~': { ++Order; Statement.at(Tokens).ndType = Standard; } break;
+				case '#': { ++Order; Statement.at(Tokens).ndType = Number; } break;
+				case '"': { ++Order; Statement.at(Tokens).ndType = String; } break;
+				default: { Statement.at(Tokens).Content += Line.at(i); }
+			}
+			break;
+			case 0:
 			switch (Line.at(i)) {
 				case '#': { Token token(Number); Statement.push_back(token); } break;
 				case '"': { Token token(String); Statement.push_back(token); } break;
 				case '$': { Token token(Variable); Statement.push_back(token); } break;
+				case '%': { Token token(Operator); Statement.push_back(token); } break;
+				case '@': { Token token(Function); Statement.push_back(token); } break;
+				case '|': { Token token(Condition); Statement.push_back(token); } break;
 			}
+			break;
 		}
 	}
 }
@@ -58,13 +78,14 @@ int main(int argc, char* argv[]) {
 		std::vector<Token> Statement;
 		Code.push_back(Statement);
 		Parse(Line, Code.at(Statements));
-		Condition = 0;
+		Order = 0;
 		Tokens = -1;
 	}
 	
 	for (std::vector<Token> statement : Code) {
 		for (Token token : statement)
-		{ std::cout << token.Type << token.Content; }
+		{ std::cout << token.stType << token.ndType << token.rdType << token.Content; }
+		std::cout << std::endl;
 	}
 	std::cout << std::endl;
 	
